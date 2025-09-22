911bac9e5d4e4a1892bb9e75198a0f5341472f0a
subham sharrma <subhamsharrma@gmail.com>
2025-09-22 11:16:02 +0530

    Initial commit

---
+++ week 1/todo.js
@@ -0,0 +1,83 @@
+// End point for todo list application
+// Get 
+// put 
+// post 
+// delete
+
+
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+// accio job js question for web and backned 
+
+
+for (let i = 0; i < 5 ; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 2000);
+}
+
